caeef347a7eb323bf582d102429a9fb0ae50f07b
MyNameLHH <1611657345@qq.com>
2022-07-07 23:19:51 +0800

    提交类和对象的练习题

---
+++ src/main/java/top/lhhstudy/Item.java
@@ -0,0 +1,34 @@
+package top.lhhstudy;
+
+public class Item {
+    /*
+    * 设计出物品这种类
+    类名：Item
+    物品有如下属性:
+    名字 name 类型是字符串String
+    价格 price 类型是整型 int
+
+    创建(实例化)3件具体物品
+    名称 价格
+    血瓶 50
+    草鞋 300
+    长剑 350
+    */
+    String name;
+    int price;
+
+    public static void main(String[] args) {
+        Item item1 = new Item();
+        item1.name = "血瓶";
+        item1.price = 50;
+
+        Item item2 = new Item();
+        item2.name = "草鞋";
+        item2.price = 300;
+
+        Item item3 = new Item();
+        item3.name = "长剑";
+        item3.price = 350;
+
+    }
+}
