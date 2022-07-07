556607fb9a1e596754e7ef84c8898108abfbb04d
MyNameLHH <1611657345@qq.com>
2022-07-08 00:11:12 +0800

    提交类和对象的练习题

---
+++ src/main/java/top/lhhstudy/Attribute.java
@@ -0,0 +1,31 @@
+package top.lhhstudy;
+/*
+* 一个英雄可以有名称，血量，护甲，移动速度，还有 其他你想到的，作为新的属性，加入到英雄上面
+    比如
+    送了多少次
+    杀人次数
+    助攻次数
+    金钱
+    补刀数
+    攻击速度
+    杀人后说的话
+    被杀后说的话
+*
+* */
+public class Attribute {
+    String name; //名称
+    int hp; //血量
+    int armor; //护甲
+    int speed; //移动速度
+    int pcs; //送了多少个
+
+    public static void main(String[] args) {
+        Attribute item = new Attribute();
+        item.name = "盖亚";
+        item.hp = 100;
+        item.pcs = 1;
+        item.armor = 1000;
+        item.speed = 10;
+
+    }
+}
