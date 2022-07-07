706e50be85b07a238b927eabcee751cfdbe47be4
MyNameLHH <1611657345@qq.com>
2022-07-08 00:13:39 +0800

    提交属性的练习题

--- src/main/java/top/lhhstudy/Item.java
+++ src/main/java/top/lhhstudy/Item.java
@@ -1,19 +1,19 @@
 package top.lhhstudy;
+/*
+* 设计出物品这种类
+类名：Item
+物品有如下属性:
+名字 name 类型是字符串String
+价格 price 类型是整型 int
 
+创建(实例化)3件具体物品
+名称 价格
+血瓶 50
+草鞋 300
+长剑 350
+*/
 public class Item {
-    /*
-    * 设计出物品这种类
-    类名：Item
-    物品有如下属性:
-    名字 name 类型是字符串String
-    价格 price 类型是整型 int
 
-    创建(实例化)3件具体物品
-    名称 价格
-    血瓶 50
-    草鞋 300
-    长剑 350
-    */
     String name;
     int price;
 
